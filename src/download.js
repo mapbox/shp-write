2355ba69ede2f6cabd20101892d66ac849949666
Peter Kupietz <kuuup@users.noreply.github.com>
2015-04-23 09:55:54 +0200

    Added options paramater

--- src/download.js
+++ src/download.js
@@ -1,6 +1,6 @@
 var zip = require('./zip');
 
-module.exports = function(gj) {
-    var content = zip(gj);
+module.exports = function(gj, options) {
+    var content = zip(gj, options);
     location.href = 'data:application/zip;base64,' + content;
 };
