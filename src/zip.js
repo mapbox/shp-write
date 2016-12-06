a9119c8309b783a986d057e259e280f1f309185b
Steven Chambers <steven@oseberg.io>
2016-12-06 12:25:21 -0600

    bugfix(zip): make feature presence check more resilient

--- src/zip.js
+++ src/zip.js
@@ -10,7 +10,7 @@ module.exports = function(gj, options) {
 
     [geojson.point(gj), geojson.line(gj), geojson.polygon(gj)]
         .forEach(function(l) {
-        if (l.geometries[0].length) {
+        if (l.geometries.length && l.geometries[0].length) {
             write(
                 // field definitions
                 l.properties,
