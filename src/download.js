6e186006f44d6e4985f567399f8656c19d60d756
alex116 <ichwillkeinspam@gmail.com>
2017-06-23 01:46:40 -0500

    fix download.js download function
    location.href is not supported in the future and content is only a Promise Object.

--- src/download.js
+++ src/download.js
@@ -2,5 +2,13 @@ var zip = require('./zip');
 
 module.exports = function(gj, options) {
     var content = zip(gj, options);
-    location.href = 'data:application/zip;base64,' + content;
+    content.then(function(abc){ 
+      var element = document.createElement('a');
+      element.setAttribute('href', 'data:application/zip;base64,' + abc);
+      element.setAttribute('download', options.folder+".zip");
+      element.style.display = 'none';
+      document.body.appendChild(element);
+      element.click();
+      document.body.removeChild(element);
+    });
 };
