9efb38a314a13f1d13e9c994e7113b266459d616
Ryan Sims <dulldrums09@gmail.com>
2018-12-18 20:06:23 -0500

    Add option to zip files without folder

--- src/zip.js
+++ src/zip.js
@@ -8,6 +8,11 @@ module.exports = function(gj, options) {
     var zip = new JSZip(),
         layers = zip.folder(options && options.folder ? options.folder : 'layers');
 
+    // if options.folder is set to false, zip files without a folder
+    if (options && options.folder === false) {
+        layers = zip;
+    }
+    
     [geojson.point(gj), geojson.line(gj), geojson.polygon(gj)]
         .forEach(function(l) {
         if (l.geometries.length && l.geometries[0].length) {
