8ac5d9faee8947dabaa6bdef9c9d9d93c39bb21d
Ruben van Eldik <r.vaneldik@evtools.nl>
2021-12-23 13:40:32 +0100

    Set the name of the ZIP file to the folder name, if a folder name has been specified

--- src/download.js
+++ src/download.js
@@ -2,5 +2,13 @@ var zip = require('./zip');
 
 module.exports = function(gj, options) {
     var content = zip(gj, options);
-    location.href = 'data:application/zip;base64,' + content;
+
+    var link = document.createElement('a');
+    link.href = 'data:application/zip;base64,' + content;
+    link.rel = 'noopener'
+    if (options && options.folder) {
+      link.download = options.folder + '.zip';
+    }
+    link.click();
+    delete link;
 };
