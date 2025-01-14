5bc30bedee75f37bff439cc71d6e3c6f611f25f7
Annie Cartas <acartas@umd.edu>
2025-01-14 13:33:55 -0500

    Bugfix: Multiple polyines should map to multiple features in output

--- src/zip.js
+++ src/zip.js
@@ -18,25 +18,28 @@ module.exports = function (
   var prj = (options && options.prj) ? options.prj : defaultPrj;
 
   [
-    geojson.point(gj),
-    geojson.line(gj),
-    geojson.polygon(gj),
-    geojson.multipolygon(gj),
-    geojson.multiline(gj),
-  ].forEach(function (l) {
-    if (l.geometries.length && l.geometries[0].length) {
-      write(
+    [geojson.point(gj)],
+    [geojson.line(gj), geojson.multiline(gj)],
+    [geojson.polygon(gj), geojson.multipolygon(gj)],
+    ].forEach(function (l) {
+    if ((l[0].geometries.length && l[0].geometries[0].length) || l[1] && l[1].geometries.length && l[1].geometries[0].length) {
+ 
+        const properties = l.flatMap(l => l.properties);
+        const geometries = l.flatMap(l => l.geometries);
+        const type = l[0].type;
+ 
+        write(
         // field definitions
-        l.properties,
+        properties,
         // geometry type
-        l.type,
+        l[0].type,
         // geometries
-        l.geometries,
+        geometries,
         function (err, files) {
           var fileName =
-            options && options.types && options.types[l.type.toLowerCase()]
-              ? options.types[l.type.toLowerCase()]
-              : l.type;
+            options && options.types && options.types[type.toLowerCase()]
+              ? options.types[type.toLowerCase()]
+              : type;
           zipTarget.file(fileName + ".shp", files.shp.buffer, { binary: true });
           zipTarget.file(fileName + ".shx", files.shx.buffer, { binary: true });
           zipTarget.file(fileName + ".dbf", files.dbf.buffer, { binary: true });
